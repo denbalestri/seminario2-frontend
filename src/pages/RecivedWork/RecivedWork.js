5d33f1e0e3d9029e7fcb730043aafe7d469dd1af
denbalestri <denisbalestri@hotmail.com>
2020-05-12 20:18:17 -0300

    add image not results

--- src/pages/RecivedWork/RecivedWork.js
+++ src/pages/RecivedWork/RecivedWork.js
@@ -3,13 +3,14 @@
 import React, { useState, useEffect } from "react";
 import RecivedWorkCard from "../../components/RecivedWorkCard";
 import MainLayout from "../../components/Layout";
+
 import Modal from "../../components/Modal";
 import { OBRAS_SINCORREGIR_URL } from "../../constants/URIs";
 
 const RecivedWork = () => {
   const [loading, setLoading] = useState(false);
   const [visible, setVisible] = useState(false);
-  const [recivedWork, setRecivedWork] = useState([]);
+  const [recivedWork, setRecivedWork] = useState("");
   const [autor, setAutor] = useState("");
   useEffect(() => {
     getWorks();
@@ -46,16 +47,31 @@ const RecivedWork = () => {
   };
   return (
     <MainLayout>
-      {recivedWork.map((work, index) => {
-        const recivedWorkProps = {
-          key: index,
-          openModal: openModal,
-          title: work.nombreObra,
-          author: `${work.nombreAutor} ${work.apellidoAutor}`,
-          description: `El genero de esta obra es ${work.genero}`,
-        };
-        return <RecivedWorkCard {...recivedWorkProps} />;
-      })}
+      {recivedWork ? (
+        recivedWork.map((work, index) => {
+          const recivedWorkProps = {
+            key: index,
+            openModal: openModal,
+            title: work.nombreObra,
+            author: `${work.nombreAutor} ${work.apellidoAutor}`,
+            description: `El genero de esta obra es ${work.genero}`,
+          };
+          return <RecivedWorkCard {...recivedWorkProps} />;
+        })
+      ) : (
+        <section>
+          <p
+            style={{
+              fontSize: 30,
+              marginTop: 20,
+              fontFamily: "Pangolin, cursive",
+            }}
+          >
+            Nuestro autor esta escribiendo obras estupendas, intente nuevamente!
+          </p>
+          <img src="../../../images/author.gif" alt="Sin resultados"></img>
+        </section>
+      )}
       <Modal
         visible={visible}
         autor={autor}
