735c2770a7ba4f02b7f97f8541e996db50617700
denbalestri <denisbalestri@hotmail.com>
2020-05-13 21:55:19 -0300

    fix debounce function

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,24 +1,21 @@
-/** @format */
-
-import React, { useState, useEffect } from "react";
-import { SEARCHPROFESSIONAL_URL } from "../../constants/URIs";
-import { useDispatch, useSelector } from "react-redux";
-import { setProfessionals } from "../../redux/actions/professionals";
-import { Input } from "antd";
-import { debounce } from "lodash";
-import { PROFESSIONALS_PATH } from "../../constants/PathNames";
+import React, { useState, useEffect } from 'react';
+import { SEARCHPROFESSIONAL_URL } from '../../constants/URIs';
+import { useDispatch } from 'react-redux';
+import { setProfessionals } from '../../redux/actions/professionals';
+import { Input } from 'antd';
+import { debounce } from 'lodash';
+import { PROFESSIONALS_PATH } from '../../constants/PathNames';
 const { Search } = Input;
 
 const MainLayout = ({ children }) => {
-  const [placeholder, setPlaceholder] = useState("");
+  const [placeholder, setPlaceholder] = useState('');
   const [searchHide, setSearchHide] = useState(false);
   const [loading, setLoading] = useState(false);
-  const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
 
   const checkPath = () => {
     if (window.location.pathname === PROFESSIONALS_PATH) {
-      setPlaceholder("Busqueda por profesional");
+      setPlaceholder('Busqueda por profesional');
     } else {
       setSearchHide(true);
     }
@@ -28,59 +25,61 @@ const MainLayout = ({ children }) => {
     checkPath();
   }, []);
 
-  const onSearch = (professional) => {
+  const onSearch = debounce(professional => {
+    if (professional === '') return;
+
     setLoading(true);
-    fetch(SEARCHPROFESSIONAL_URL(user.username), {
-      method: "POST",
-      mode: "no-cors",
+    fetch(SEARCHPROFESSIONAL_URL(professional), {
+      method: 'GET',
+      mode: 'cors',
       headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
+        'Content-Type': 'application/x-www-form-urlencoded',
       },
-      body: professional,
     })
-      .then((response) => {
+      .then(response => {
         const professionals = response.data;
         dispatch(setProfessionals(professionals));
       })
-      .catch((error) => console.log(error))
+      .catch(error => console.log(error))
       .finally(() => setLoading(false));
-  };
+  }, 500);
 
-  const onChange = debounce((professional) => {
-    if (professional !== "") onSearch(professional);
-  }, 1000);
+  const onChange = e => {
+    const professional = e.target.value;
+    onSearch(professional);
+  };
 
   return (
     <main
       style={{
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "stretch",
-        flexDirection: "column",
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'stretch',
+        flexDirection: 'column',
       }}
     >
       <nav
         style={{
           height: 50,
           padding: 10,
-          width: "100%",
-          backgroundColor: "#CCC",
-          display: "flex",
-          justifyContent: "center",
+          width: '100%',
+          backgroundColor: '#CCC',
+          display: 'flex',
+          justifyContent: 'center',
         }}
       >
         {!searchHide ? (
           <Search
             placeholder={placeholder}
-            onSearch={(professional) => onSearch(professional)}
+            onSearch={professional => onSearch(professional)}
             style={{ width: 400 }}
             loading={loading}
             onChange={onChange}
           />
         ) : (
-          ""
+          ''
         )}
       </nav>
       {children}
